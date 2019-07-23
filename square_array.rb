2b64d37bc9fa4abbe2be3b9ffa258828930e99a9
Annie Truex <artruex@gmail.com>
2019-07-23 18:42:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   squared = []
   array.each do |element| 
-    puts element ** 2 
+    puts squared << element ** 2 
   end
-  squared << element 
+  #squared << 
 end
